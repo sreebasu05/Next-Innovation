0c2e54268d59ab1faf1c46030299bac94574d949
sreebasu05 <60813392+sreebasu05@users.noreply.github.com>
2020-05-16 22:43:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const _ = require("lodash");
 const mongoose = require('mongoose');
 
 const app = express();
+var favicon = require('serve-favicon');
 
+app.use('/favicon.ico', express.static('/favicon.ico'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -50,6 +52,18 @@ const mailsSchema = new mongoose.Schema({
 
 const Mail = mongoose.model("Mail", mailsSchema);
 
+
+const usersSchema = new mongoose.Schema({
+  username: String,
+  fullname: String,
+  password: String,
+  email: String,
+  books: [booksSchema]
+});
+
+const User = mongoose.model("User", usersSchema);
+
+const defaultBooks = [];
 app.get("/items", function(req, res) {
   Book.find({}, function(err, foundBooks) {
 
@@ -72,6 +86,110 @@ app.get("/items", function(req, res) {
     }
   });
 });
+app.get("/dashboard/:user", function(req, res) {
+  const user = req.params.user;
+  User.findOne({
+    username: user
+  }, function(err, foundUser) {
+    if (!err) {
+      res.render("dashboard", {
+        user: user,
+        newBookItems: foundUser.books
+      });
+    }
+  });
+});
+app.post("/delete/:user", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  const user = req.params.user;
+
+  Book.findByIdAndRemove(checkedItemId, function(err) {
+    if (!err) {
+      console.log("Success");
+    }
+  });
+  User.findOneAndUpdate({
+      username: user
+    }, {
+      $pull: {
+        books: {
+          _id: checkedItemId
+        }
+      }
+    },
+    function(err, foundList) {
+      if (!err) {
+        res.redirect("/dashboard/" + user);
+      }
+    });
+});
+
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+app.post("/signup", function(req, res) {
+  if (req.body.password === req.body.cpassword) {
+    const user = new User({
+      username: req.body.username,
+      fullname: req.body.fullname,
+      password: req.body.password,
+      email: req.body.email,
+      books: defaultBooks
+
+    });
+    const u = req.body.username;
+    user.save();
+    res.redirect("/index/" + u);
+  } else {
+    console.log("error");
+    res.redirect("/signup");
+  }
+});
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+app.post("/login", function(req, res) {
+  const user = req.body.username
+  User.findOne({
+    username: user
+  }, function(err, foundUser) {
+    if(!err){
+      pass=foundUser.password;
+    if ( pass === req.body.password) {
+      res.redirect("/index/" + user);
+    } else {
+      console.log("Wrong Password");
+      res.render("/index"+user);
+    }}
+  });
+});
+app.get("/index/:user", function(req, res) {
+  const user = req.params.user;
+  User.findOne({
+    username: user
+  }, function(err, foundUser) {
+    res.render("indexB", {
+      name: foundUser.fullname,
+      user: foundUser.username
+    });
+  });
+});
+//
+
+
+
+
+app.get("/add/:user", function(req, res) {
+  const user = req.params.user;
+  User.findOne({
+    username: user
+  }, function(err, foundUser) {
+    if(!err){
+    res.render("additem", {
+      user: foundUser.username
+    });}
+  });
+});
 
 
 app.get("/add", function(req, res) {
@@ -79,7 +197,8 @@ app.get("/add", function(req, res) {
   res.render("additem");
 });
 
-app.post("/add", function(req, res) {
+app.post("/add/:user", function(req, res) {
+  const user = req.params.user;
   const book = new Book({
     bookname: req.body.bookname,
     sellp: req.body.sellp,
@@ -95,9 +214,13 @@ app.post("/add", function(req, res) {
     room: req.body.room
   });
   book.save();
-  res.redirect("/items");
-
-
+  User.findOne({
+    username: user
+  }, function(err, foundUser) {
+    foundUser.books.push(book);
+    foundUser.save();
+    res.redirect("/dashboard/" + user);
+  });
 });
 
 app.get("/items/:bookid", function(req, res) {
